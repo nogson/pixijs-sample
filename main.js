77c2bacef9530e3ae676b352bd57efd3dbde9caa
佐藤文彦 <fsato@ciaoinc.jp>
2021-12-27 21:33:07 +0900

    サンプル

---
+++ main.js
@@ -0,0 +1,31 @@
+const main = () => {
+    let app = new PIXI.Application({
+        width: 500,
+        height: 500,
+        resolution: window.devicePixelRatio || 1,
+        autoResize: true,
+        backgroundColor: 0xFFFFFF
+    });
+    document.body.appendChild(app.view);
+
+    const animatedSprite = animation()
+    animatedSprite.play()
+    app.stage.addChild(animatedSprite);
+
+}
+
+const animation = () => {
+    const textureArray = []
+    for (let i = 0; i < 30; i++) {
+        const name = ('000' + (i + 1)).slice(-4)
+        let texture = PIXI.Texture.from('images/' + name +'.png');
+        textureArray.push(texture);
+    }
+
+    let animatedSprite = new PIXI.AnimatedSprite(textureArray);
+    animatedSprite.animationSpeed = 0.2;
+    return animatedSprite
+}
+
+
+document.addEventListener('DOMContentLoaded', () => main())
